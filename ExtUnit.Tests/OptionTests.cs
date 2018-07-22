f106f19c985587cf1fd8a085a6de98fcdd72788f
Tom Cole <colethecoder@gmail.com>
2018-07-22 09:42:26 +0100

    Quick mock up of Option asserts

---
+++ ExtUnit.Tests/OptionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LanguageExt;
+using static LanguageExt.Prelude;
+using Xunit;
+using Xunit.Sdk;
+
+namespace ExtUnit.Tests
+{
+    public class OptionTests
+    {
+        [Fact]
+        public void IsNone_Pass() =>
+            Assert.IsNone(Option<string>.None);
+
+        [Fact]
+        public void IsNone_Fail() =>
+            Assert.Throws<AssertActualExpectedException>(
+                () => Assert.IsNone(Optional("1234")));
+        
+    }
+}
---
+++ ExtUnit/ExtendedAsserts/OptionAsserts.cs
@@ -0,0 +1,37 @@
+﻿using LanguageExt;
+using static LanguageExt.Prelude;
+using Xunit.Sdk;
+
+namespace Xunit
+{
+#if XUNIT_VISIBILITY_INTERNAL 
+    internal
+#else
+    public
+#endif
+    partial class Assert
+    {
+        /// <summary>
+        /// Verifies that a string contains a given sub-string, using the current culture.
+        /// </summary>
+        /// <param name="expectedSubstring">The sub-string expected to be in the string</param>
+        /// <param name="actualString">The string to be inspected</param>
+        /// <exception cref="ContainsException">Thrown when the sub-string is not present inside the string</exception>
+        public static void IsNone<T>(Option<T> value) =>
+            value.Match(
+                Some: x => throw new AssertActualExpectedException(Option<T>.None, value, "Expected None but the Option had a value."),
+                None: () => unit);
+
+        public static void IsSome<T>(Option<T> value) =>
+            value.Match(
+                Some: identity,
+                None: () => throw new AssertActualExpectedException(Option<T>.Some(default(T)), value, "Expected Some but the Option was None."));
+
+        public static void IsSome<T>(T expected, Option<T> actual) =>
+            actual.Match(
+                Some: x => Assert.Equal(expected, x),
+                None: () => throw new AssertActualExpectedException(Option<T>.Some(default(T)), actual, "Expected Some but the Option was None.")
+                );
+
+    }
+}
