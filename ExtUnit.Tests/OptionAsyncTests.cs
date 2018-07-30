73bb14b3c0b46874fc067a281b4f5aea712789d5
Tom Cole <colethecoder@gmail.com>
2018-07-30 21:46:42 +0100

    OptionAsync tests added

---
+++ ExtUnit.Tests/OptionAsyncTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LanguageExt;
+using static LanguageExt.Prelude;
+using Xunit;
+using Xunit.Sdk;
+
+namespace ExtUnit.Tests
+{
+    public class OptionAsyncTests
+    {
+        [Fact]
+        public async void NoneAsync_Pass() =>
+            await Assert.NoneAsync(OptionAsync<string>.None);
+
+        [Fact]
+        public async void NoneAsync_Fail() =>
+            await Assert.ThrowsAsync<NoneException>(
+                async () => await Assert.NoneAsync(OptionAsync<string>.Some("1234")));
+
+        [Fact]
+        public async void SomeAsync_Pass() =>
+            await Assert.SomeAsync(OptionAsync<string>.Some("1234"));
+
+        [Fact]
+        public async void SomeAsync_Fail() =>
+            await Assert.ThrowsAsync<SomeException>(
+                async () =>  await Assert.SomeAsync(OptionAsync<string>.None));
+
+        [Fact]
+        public async void SomeAsync_WithExpectation_Pass() =>
+            await Assert.SomeAsync("1234", OptionAsync<string>.Some("1234"));
+
+        [Fact]
+        public async void SomeAsync_WithNone_Fail() =>
+            await Assert.ThrowsAsync<SomeException>(
+                async () => await Assert.SomeAsync("1234", OptionAsync<string>.None));
+
+        [Fact]
+        public async void Some_WithIncorrectExpectation_Fail() =>
+            await Assert.ThrowsAsync<EqualException>(
+                async () => await Assert.SomeAsync("1234", OptionAsync<string>.Some("5678")));
+
+        [Fact]
+        public async void SomeAsync_WithCorrectAsserts_Pass() =>
+            await Assert.SomeAsync(OptionAsync<int>.Some(1234), x => Assert.InRange(x, 1, 9999));
+
+        [Fact]
+        public async void SomeAsync_WithIncorrectAsserts_Fail() =>
+            await Assert.ThrowsAsync<InRangeException>(
+                async () => await Assert.SomeAsync(OptionAsync<int>.Some(1234), x => Assert.InRange(x, 1, 999)));
+    }
+}
---
+++ ExtUnit/ExtendedAsserts/OptionAsyncAsserts.cs
@@ -0,0 +1,53 @@
+﻿using LanguageExt;
+using static LanguageExt.Prelude;
+using Xunit.Sdk;
+using System;
+using System.Threading.Tasks;
+
+namespace Xunit
+{
+#if XUNIT_VISIBILITY_INTERNAL 
+    internal
+#else
+    public
+#endif
+    partial class Assert
+    {
+        /// <summary>
+        /// Verifies that an Option is in the None state
+        /// </summary>
+        /// <param name="value">The Option to be assessed</param>        
+        /// <exception cref="NoneException">Thrown when the Option is in the Some state</exception>
+        public async static Task NoneAsync<T>(OptionAsync<T> value) =>
+            await value.Match(
+                Some: x => throw new NoneException(),
+                None: () => unit);
+
+        /// <summary>
+        /// Verifies that an Option is in the Some state
+        /// </summary>
+        /// <param name="value">The Option to be assessed</param>        
+        /// <exception cref="SomeException">Thrown when the Option is in the None state</exception>
+        public async static Task SomeAsync<T>(OptionAsync<T> value) =>
+            await value.Match(
+                Some: identity,
+                None: () => throw new SomeException());
+
+        /// <summary>
+        /// Verifies that an Option is in the Some state and the value matches the expectation
+        /// </summary>
+        /// <param name="value">The Option to be assessed</param>        
+        /// <exception cref="SomeException">Thrown when the Option is in the None state</exception>
+        /// <exception cref="EqualException">Thrown when the Option value does not match</exception>
+        public async static Task SomeAsync<T>(T expected, OptionAsync<T> actual) =>
+            await actual.Match(
+                Some: x => Equal(expected, x),
+                None: () => throw new SomeException());
+
+        public async static Task SomeAsync<T>(OptionAsync<T> value, Action<T> asserts) =>
+            await value.Match(
+                Some: x => asserts(x),
+                None: () => throw new SomeException());
+
+    }
+}
